191aadbcc6d7af1f56cc853896367719c5cb83e1
pollyvs <vesninap@mail.ru>
2023-05-19 16:06:24 +0300

    8d

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,6 @@
 let images = document.querySelectorAll(".hot");
 let current=0;
 
-const rightarrow=document.querySelector('.rightarrow')
-const leftarrow=document.querySelector('.leftarrow')
 
 function slider(){
     for(let i=0;i<images.length;i++){
@@ -19,43 +17,3 @@ function slider(){
     setTimeout("slider()",5000);
 }
 window.onload=slider;
-// document.querySelector('.rightarrow').onclick=function(){
-//     if (current-1===-1){
-//         current=images.length-1;
-//     }
-//     else{
-//         current--;
-//     }
-//     slider();
-// };
-// document.querySelector('.leftarrow').onclick=function (){
-//     if (current+1===images.length){
-//         current=0;
-//     }
-//     else{
-//         current++;
-//     }
-//     slider();
-// };
-// document.addEventListener('keyup', function(event){
-//     if(event.key==="ArrowRight"){
-//         if (current-1===-1){
-//             current=images.length-1;
-//         }
-//         else{
-//             current--;
-//         }
-//         slider();
-//     }
-//     else if(event.key==="ArrowLeft"){
-//         if (current+1===images.length){
-//             current=0;
-//         }
-//         else{
-//             current++;
-//         }
-//         slider();
-//     }
-//
-//
-// });
